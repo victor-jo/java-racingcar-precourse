dfa04de115d9edf5b0dd936664afda315959e4d7
victor-jo <joasjn45@gmail.com>
2022-10-09 13:42:20 +0900

    feat (io) : console io 를 위한 컨텍스트 추가
    - 콘솔 입력
    - 콘솔 출력

---
+++ src/main/java/racingcar/context/ConsoleScanner.java
@@ -0,0 +1,28 @@
+package racingcar.context;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ConsoleScanner implements Scanner {
+
+    private static final String COMMA = ",";
+    public static final String EXCEPTION_MESSAGE = "[ERROR] 시도할 횟수가 잘못되었습니다.";
+
+    @Override
+    public List<String> getNames() {
+        return Arrays.asList(Console.readLine().split(COMMA));
+    }
+
+    @Override
+    public int getTry(Printer errPrinter) {
+        try {
+            return Integer.parseInt(Console.readLine());
+        }
+        catch (Exception e) {
+            errPrinter.println(EXCEPTION_MESSAGE);
+            throw new IllegalArgumentException(EXCEPTION_MESSAGE);
+        }
+    }
+}
---
+++ src/main/java/racingcar/context/Scanner.java
@@ -0,0 +1,8 @@
+package racingcar.context;
+
+import java.util.List;
+
+public interface Scanner {
+    List<String> getNames();
+    int getTry(Printer errPrinter);
+}
